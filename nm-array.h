88fc314d27e4e8bd667a7f1bc0512dd1c54d7daf
reznikovkg <reznikov.kosta@yandex.ru>
2018-03-19 22:02:21 +0300

    first release

---
+++ nm-array.h
@@ -0,0 +1,17 @@
+#ifndef REZNIKOKG_IPN_H
+
+/*
+	Author: reznikovkg
+	GitHub: https://github.com/reznikovkg
+	Email: kosrez1@yandex.ru
+
+	Project name: Interpolation polinom by Newton
+	File name: nm-array.h
+	GitHub Repository: https://github.com/reznikovkg/nm-ipN
+*/
+
+#define REZNIKOKG_IPN_H
+
+#include "include/nm_array_class.h"
+
+#endif REZNIKOKG_IPN_H
